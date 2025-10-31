097c0efae9b54f0adfb0d37eb0c3b3f27b88ba17
PrajwalM7 <prajwalm7781@gmail.com>
2025-10-31 13:35:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import { Booking } from "./models/Booking.js";
 dotenv.config(); // Load .env
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 connectDB();
